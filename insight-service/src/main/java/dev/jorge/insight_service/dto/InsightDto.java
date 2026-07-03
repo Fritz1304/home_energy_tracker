8a5a26bfb40ed557dadcd0af2d12bec1bde94b33
Jorge Gamboa <jorgegamboa@192.168.1.10>
2026-07-03 11:37:07 -0500

    feat: IA module finish

---
+++ insight-service/src/main/java/dev/jorge/insight_service/dto/InsightDto.java
@@ -0,0 +1,12 @@
+package dev.jorge.insight_service.dto;
+
+
+import lombok.Builder;
+
+@Builder
+public record InsightDto(
+        Long userId,
+        String tips,
+        double energyUsage
+) {
+}
---
+++ insight-service/src/main/java/dev/jorge/insight_service/dto/UsageDto.java
@@ -0,0 +1,12 @@
+package dev.jorge.insight_service.dto;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record UsageDto(
+        Long userId,
+        List<DeviceDto> devices
+) {
+}
